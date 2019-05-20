26910aabfcdff9052405485a85ab274ac2428c89
arodr856 <arodr856@gmail.com>
2019-05-20 00:54:17 -0700

    changed time from 24 hours to 12 hours and day of the week is being displayed

--- app/index.js
+++ app/index.js
@@ -1,15 +1,33 @@
 import document from 'document';
 import clock from 'clock';
 
+import { days } from './js/date-data';
+
 const clockText = document.getElementById('time');
 const dateText = document.getElementById('date');
+const dayText = document.getElementById('day');
 
 // clock set up
 clock.granularity = 'seconds';
 
 clock.ontick = (event) => {
-  const currentTime = `${event.date.getHours()}:${event.date.getMinutes()}:${event.date.getSeconds()}`;
+  const currentTime = `${modHours(event.date.getHours())}:${event.date.getMinutes()}:${event.date.getSeconds()}`;
   const currentDate = `${event.date.getMonth() + 1} / ${event.date.getDate()}`;
   dateText.text = currentDate;
   clockText.text = currentTime;
+  dayText.text = days[event.date.getDay()];
+}
+
+const modHours = (hour) => {
+
+  const moddedHour = hour % 12;
+
+  if(moddedHour < 10){
+    if(moddedHour === 0)
+      return '01';
+    return (0 + moddedHour);
+  }else{
+    return moddedHour;
+  }
+
 }
--- app/js/date-data.js
+++ app/js/date-data.js
@@ -1,7 +1,7 @@
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const months = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER'];
 const abbreviatedMonths = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-module.exports = {
+export {
     days,
     months,
     abbreviatedMonths
